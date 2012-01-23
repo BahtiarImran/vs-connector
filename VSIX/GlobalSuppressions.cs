ed86ec4b3a01c7f28866d169bdadcba3eda50fd6
mrichter <mark.richter@thoughtworks.com>
2012-01-23 13:19:36 -0800

    suppress compiler warning requesting more specific exception type in VsPkg

--- VSIX/GlobalSuppressions.cs
+++ VSIX/GlobalSuppressions.cs
@@ -497,3 +497,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1056:UriPropertiesShouldNotBeStrings", Scope = "member", Target = "ThoughtWorks.VisualStudio.Card.#ModifiedByUrl")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1056:UriPropertiesShouldNotBeStrings", Scope = "member", Target = "ThoughtWorks.VisualStudio.Card.#CreatedByUrl")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1056:UriPropertiesShouldNotBeStrings", Scope = "member", Target = "ThoughtWorks.VisualStudio.Card.#CardTypeUrl")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Scope = "member", Target = "ThoughtWorks.VisualStudio.TwVscCommandsPackage.#ShowMingleExplorer(System.Object,System.EventArgs)")]
