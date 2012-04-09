4f4d2e749b751794d747d3854d1c0672f36e9ec9
markdrichter <mark@markr.com>
2012-04-09 08:52:25 -0700

    adjust margins around buttons

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-@echo off
 set path=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;
 msbuild /t:Rebuild /p:configuration=%1 /fileLogger /flp:logfile=buildlog.txt /flp1:logfile=builderrors.txt;errorsonly /flp2:logfile=buildwarnings.txt;warningsonly vsc.sln
 
