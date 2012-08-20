abdd6a21432508dad0d8d4bb24c26108fb82fbf7
Mark Richter <mrichter@thoughtworks.com>
2012-08-20 12:37:36 -0700

    #71 specify full path to msbuild

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
-set path=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;
-msbuild /t:Rebuild /p:configuration=%1 /fileLogger /flp:logfile=buildlog.txt /flp1:logfile=builderrors.txt;errorsonly /flp2:logfile=buildwarnings.txt;warningsonly vsc.sln
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /t:Rebuild /p:configuration=%1 /fileLogger /flp:logfile=buildlog.txt /flp1:logfile=builderrors.txt;errorsonly /flp2:logfile=buildwarnings.txt;warningsonly vsc.sln
 
