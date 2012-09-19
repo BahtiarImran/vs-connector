eabdfca3aa45cd33733009e8490f6743b25db13c
Mark Richter <mrichter@thoughtworks.com>
2012-09-19 12:53:00 -0700

    #76 switch to building from msbuild project not sln

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /t:Rebuild /p:configuration=%1 /fileLogger /flp:logfile=buildlog.txt /flp1:logfile=builderrors.txt;errorsonly /flp2:logfile=buildwarnings.txt;warningsonly vsc.sln
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /t:Rebuild /p:configuration=%1 /fileLogger /flp:logfile=buildlog.txt /flp1:logfile=builderrors.txt;errorsonly /flp2:logfile=buildwarnings.txt;warningsonly vsc.proj
 
