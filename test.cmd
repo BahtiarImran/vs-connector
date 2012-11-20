702ec102b4dd7a7f92c47ad6c1a619b3f7829d85
Mark Richter <mrichter@thoughtworks.com>
2012-11-20 04:37:47 -0800

    refine the test script

--- test.cmd
+++ test.cmd
@@ -1,3 +1,9 @@
-del /S build\tests\testresults.trx
+@ECHO OFF
+DEL /S build\tests\testresults.trx
+ECHO Beginning test run.
 %MSTEST% /testcontainer:build\tests\VsConnectorTests.dll /resultsfile:build\tests\testresults.trx
+ECHO Test run completed. 
+ECHO Beginning test result transformation to XUnit.
 tools\msxsl build\tests\testresults.trx tools\MSBuild-to-NUnit.xslt -o build/tests/testresults.trx.xml
+ECHO Test result transformation to XUnit completed.
+ECHO Test pass ended.
