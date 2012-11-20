f6d14fbd3a04f94a84a4645028d2e7b4fb82d2ac
Mark Richter <mrichter@thoughtworks.com>
2012-11-19 17:43:13 -0800

    fix del command

--- test.cmd
+++ test.cmd
@@ -1,3 +1,3 @@
-build\tests\testresults.trx
+del /S build\tests\testresults.trx
 %MSTEST% /testcontainer:build\tests\VsConnectorTests.dll /resultsfile:build\tests\testresults.trx
 tools\msxsl build\tests\testresults.trx tools\MSBuild-to-NUnit.xslt -o build/tests/testresults.trx.xml
